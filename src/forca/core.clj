46cf0ec82be78cad74f2ae9a9ee7501cbb7b4cd5
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-07-19 13:59:51 -0300

    lê do teclado, chama java, conj

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -11,9 +11,18 @@
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
 
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)))
+
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
       (ganhou)
-      (print "Chuta, amigo!"))))
+      (avalia-chute (le-letra!) vidas palavra acertos))))
