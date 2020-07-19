ae25bd15a3a143ebe144ca36110336bbe1200647
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-07-19 13:53:06 -0300

    higher order functions, map/remove, sets, contains?, empty?

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,1 +1,19 @@
 (ns forca.core)
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
+
+(defn letras-faltantes [palavra acertos]
+ (remove (fn [letra] (contains? acertos (str letra))) palavra) )
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn jogo [vidas palavra acertos]
+  (if (= vidas 0)
+    (perdeu)
+    (if (acertou-a-palavra-toda? palavra acertos)
+      (ganhou)
+      (print "Chuta, amigo!"))))
