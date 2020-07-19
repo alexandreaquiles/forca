583eacc821521edf938813b27cf51aca53097851
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-07-19 14:06:50 -0300

    tail call optimization e recur

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -6,7 +6,7 @@
 (defn ganhou [] (print "Você ganhou!"))
 
 (defn letras-faltantes [palavra acertos]
- (remove (fn [letra] (contains? acertos (str letra))) palavra) )
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
@@ -15,14 +15,14 @@
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)))
-
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos))))
+  (cond (= vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else (let [chute (le-letra!)]
+                (if (acertou? chute palavra)
+                  (do
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute)))
+                  (do
+                    (println "Errou a letra! Perdeu a vida!")
+                    (recur (dec vidas) palavra acertos))))))
